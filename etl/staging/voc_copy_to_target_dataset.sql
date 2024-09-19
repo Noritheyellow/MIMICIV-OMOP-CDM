a905a5b76f51a43f06a64db5fbe65478bb2b5d8d
noritheyellow <noritheyellow@gmail.com>
2024-09-19 10:31:38 +0000

    update: voc staging

--- etl/staging/voc_copy_to_target_dataset.sql
+++ etl/staging/voc_copy_to_target_dataset.sql
@@ -49,3 +49,55 @@ CREATE OR REPLACE TABLE `@etl_project`.@etl_dataset.voc_drug_strength AS
 SELECT * FROM `@voc_project`.@voc_dataset.drug_strength
 ;
 
+
+-- -------------------------------------------------------------------
+-- 2024.09.20. David Hwang.
+-- Modify above query fit into our project 'AEGIS'.
+-- -------------------------------------------------------------------
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_concept;
+CREATE TABLE mimic_omop_cdm.voc_concept AS
+SELECT * FROM standardized_vocabularies.concept
+;
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_concept_relationship;
+CREATE TABLE mimic_omop_cdm.voc_concept_relationship AS
+SELECT * FROM standardized_vocabularies.concept_relationship
+;
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_vocabulary;
+CREATE TABLE mimic_omop_cdm.voc_vocabulary AS
+SELECT * FROM standardized_vocabularies.vocabulary
+;
+
+-- not affected by custom mapping
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_domain;
+CREATE TABLE mimic_omop_cdm.voc_domain AS
+SELECT * FROM standardized_vocabularies."DOMAIN"
+;
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_concept_class;
+CREATE TABLE mimic_omop_cdm.voc_concept_class AS
+SELECT * FROM standardized_vocabularies.concept_class
+;
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_relationship;
+CREATE TABLE mimic_omop_cdm.voc_relationship AS
+SELECT * FROM standardized_vocabularies.relationship
+;
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_concept_synonym;
+CREATE TABLE mimic_omop_cdm.voc_concept_synonym AS
+SELECT * FROM standardized_vocabularies.concept_synonym
+;
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_concept_ancestor;
+CREATE TABLE mimic_omop_cdm.voc_concept_ancestor AS
+SELECT * FROM standardized_vocabularies.concept_ancestor
+;
+
+DROP TABLE IF EXISTS mimic_omop_cdm.voc_drug_strength;
+CREATE TABLE mimic_omop_cdm.voc_drug_strength AS
+SELECT * FROM standardized_vocabularies.drug_strength
+;
